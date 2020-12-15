3113775641c5d1f2c9238f6896be953328419a3f
Harry <harryguloien@gmail.com>
2020-12-15 18:08:33 -0500

    adding photos and JS

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const myApp = {}
 
+myApp.init = () => {
+    // scroll down fade up plugin
+    AOS.init({
+        easing: 'ease-in-quad',
+    });
+
+    $('.lightSwitch').on('click', function() {
+        console.log("we clicked");
+        $('body').toggleClass('darkMode');
+        $('div.line').toggleClass('darkLine')
+    })
+}
+
+
+
+
+  $(document).ready(function() {
+    myApp.init();
+  });
