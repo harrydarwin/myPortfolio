27d48929bd60ea29b228fb5e9969a82e3918a82b
Harry Darwin <harryguoien@gmail.com>
2023-06-14 10:46:19 -0400

    ermoved all checks for postman test

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -21,12 +21,12 @@ exports.handler = async function (event, context) {
   console.log('Payload recieved!')
   let response = {};
   const requestBody = event.body ? JSON.parse(event.body) : null;
-if (!requestBody) {
-  return {
-    statusCode: 400,
-    body: JSON.stringify({ message: 'Invalid request body.' })
-  };
-}
+  if (!requestBody) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Invalid request body.' })
+    };
+  }
   // Only accept ROBS payloads + only transcript end events
   // if(requestBody.event !== 'recording.transcript_completed' || requestBody.payload.object.host_id !== 'i50cPqx3R22xnUS0I6ZVOw'){
   //   return;
@@ -37,28 +37,28 @@ if (!requestBody) {
   console.log('Body', requestBody);
 
 
-  const message = `v0:${event.headers['x-zm-request-timestamp']}:${JSON.stringify(requestBody)}`;
+  // const message = `v0:${event.headers['x-zm-request-timestamp']}:${JSON.stringify(requestBody)}`;
 
-  const hashForVerify = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(message).digest('hex');
+  // const hashForVerify = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(message).digest('hex');
 
-  const signature = `v0=${hashForVerify}`;
+  // const signature = `v0=${hashForVerify}`;
 
-  if (event.headers['x-zm-signature'] === signature) {
-    console.log("HEADER THING MATCHED");
+  // if (event.headers['x-zm-signature'] === signature) {
+  //   console.log("HEADER THING MATCHED");
 
-    if (requestBody.event === 'endpoint.url_validation') {
-      const hashForValidate = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(requestBody.payload.plainToken).digest('hex');
+    // if (requestBody.event === 'endpoint.url_validation') {
+    //   const hashForValidate = crypto.createHmac('sha256', process.env.ZOOM_WEBHOOK_SECRET_TOKEN).update(requestBody.payload.plainToken).digest('hex');
 
-      response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          plainToken: requestBody.payload.plainToken,
-          encryptedToken: hashForValidate
-        })
-      };
+    //   response = {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //       plainToken: requestBody.payload.plainToken,
+    //       encryptedToken: hashForValidate
+    //     })
+    //   };
 
-      console.log(response.body);
-    } else {
+    //   console.log(response.body);
+    // } else {
       response = {
         statusCode: 200,
         body: JSON.stringify({ message: 'Authorized request to Zoom Webhook sample.' })
@@ -104,15 +104,15 @@ if (!requestBody) {
       console.log(thing);
 
       // business logic here, example make API request to Zoom or 3rd party
-    }
-  } else {
-    response = {
-      statusCode: 401,
-      body: JSON.stringify({ message: 'Unauthorized request to Zoom Webhook sample.' })
-    };
+    // }
+  // } else {
+  //   response = {
+  //     statusCode: 401,
+  //     body: JSON.stringify({ message: 'Unauthorized request to Zoom Webhook sample.' })
+  //   };
 
-    console.log(response.body);
-  }
+  //   console.log(response.body);
+  // }
 
   return response;
 };
