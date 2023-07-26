8eeef219ad44ee3ade4334be7d3837e93d3a7c5f
Harry Darwin <harryguoien@gmail.com>
2023-07-26 15:39:37 -0400

    Re-added Robs ID check to only get his conversations

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -30,7 +30,7 @@ exports.handler = async function (event, context) {
   }
   console.log('event TYPE: ', requestBody.event)
   // Only accept ROBS payloads + only transcript end events
-  if(requestBody.event !== 'recording.transcript_completed'){
+  if(requestBody.event !== 'recording.transcript_completed' || requestBody.payload.object.host_id !== 'i50cPqx3R22xnUS0I6ZVOw'){
     return;
   }
   console.log('EVENT BABY:', event);
